d24f6d7a594f189b04475fe976c13a122997242f
wyt <168687208+wytlines100@users.noreply.github.com>
2025-02-24 16:54:36 -0800

    Add files via upload

---
+++ Unverifed x zPoshful/background.js
@@ -0,0 +1,138 @@
+const URL_MAP = {
+  "https://miniblox.io/textures/spritesheet.png": "https://raw.githubusercontent.com/wytlines100/skibidi-textures/main/rah.png",
+  "https://miniblox.io/assets/Miniblox-Regular-Lz-C8nWS.otf": "https://cdn.glitch.global/adb12490-d563-43cb-9711-2a69a8bb1c06/Faithful.ttf?v=1735593093308",
+  "https://miniblox.io/textures/entity/chicken/chicken.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/chicken.png",
+  "https://miniblox.io/textures/entity/cow/cow.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/cow.png",
+  "https://miniblox.io/textures/entity/creeper/creeper.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/creeper.png",
+  "https://miniblox.io/textures/entity/pig/pig.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/pig.png", 
+  "https://miniblox.io/textures/entity/sheep/sheep_fur.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/sheep_fur.png",
+  "https://miniblox.io/textures/entity/skeleton/skeleton.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/skeleton.png",
+  "https://miniblox.io/textures/entity/slime/slime.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/slime.png",
+  "https://miniblox.io/textures/entity/spider/spider.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/spider.png",
+  "https://miniblox.io/textures/entity/zombie/zombie.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/zombie.png",
+  "https://miniblox.io/textures/entity/ghost/ghost.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/ghost.png",
+  "https://miniblox.io/textures/entity/zombie_cowman/zombie_cowman.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/zombie_cowman.png",
+  "https://miniblox.io/textures/entity/snowman/snowman.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/snowman.png",
+  "https://miniblox.io/textures/entity/arrow.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/arrow.png",
+  "https://miniblox.io/textures/entity/boat/acacia.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/acacia.png",
+  "https://miniblox.io/textures/entity/boat/birch.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/birch.png",
+  "https://miniblox.io/textures/entity/boat/dark_oak.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/dark_oak.png",
+  "https://miniblox.io/textures/entity/boat/jungle.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/jungle.png",
+  "https://miniblox.io/textures/entity/boat/oak.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/oak.png",
+  "https://miniblox.io/textures/entity/boat/spruce.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/spruce.png",
+  "https://miniblox.io/textures/models/armor/chainmail_layer_1.png": "https://raw.githubusercontent.com/wytlines100/skibidi-textures/main/armor/chain_1.png",
+  "https://miniblox.io/textures/models/armor/chainmail_layer_2.png": "https://raw.githubusercontent.com/wytlines100/skibidi-textures/main/armor/chain_2.png",
+  "https://miniblox.io/textures/models/armor/diamond_layer_1.png": "https://raw.githubusercontent.com/wytlines100/skibidi-textures/main/armor/diamond_1.png",
+  "https://miniblox.io/textures/models/armor/diamond_layer_2.png": "https://raw.githubusercontent.com/wytlines100/skibidi-textures/main/armor/diamond_2.png",
+  "https://miniblox.io/textures/models/armor/emerald_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/emerald_layer_1.png",
+  "https://miniblox.io/textures/models/armor/emerald_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/emerald_layer_2.png",
+  "https://miniblox.io/textures/models/armor/infernium_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/infernium_layer_1.png",
+  "https://miniblox.io/textures/models/armor/infernium_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/infernium_layer_2.png",
+  "https://miniblox.io/textures/models/armor/gold_layer_1.png": "https://raw.githubusercontent.com/wytlines100/skibidi-textures/main/armor/gold_1.png",
+  "https://miniblox.io/textures/models/armor/gold_layer_2.png": "https://raw.githubusercontent.com/wytlines100/skibidi-textures/main/armor/gold_2.png",
+  "https://miniblox.io/textures/models/armor/iron_layer_1.png": "https://raw.githubusercontent.com/wytlines100/skibidi-textures/main/armor/iron_1.png",
+  "https://miniblox.io/textures/models/armor/iron_layer_2.png": "https://raw.githubusercontent.com/wytlines100/skibidi-textures/main/armor/iron_2.png",
+  "https://miniblox.io/textures/models/armor/leather_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/leather_layer_1.png",
+  "https://miniblox.io/textures/models/armor/leather_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/leather_layer_2.png",
+  "https://miniblox.online/textures/spritesheet.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/spritesheet.png",
+  "https://miniblox.online/assets/Miniblox-Regular-Lz-C8nWS.otf": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/assets/Miniblox-Regular-Lz-C8nWS.otf",
+  "https://miniblox.online/textures/entity/chicken/chicken.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/chicken.png",
+  "https://miniblox.online/textures/entity/cow/cow.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/cow.png",
+  "https://miniblox.online/textures/entity/creeper/creeper.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/creeper.png",
+  "https://miniblox.online/textures/entity/pig/pig.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/pig.png", 
+  "https://miniblox.online/textures/entity/sheep/sheep_fur.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/sheep_fur.png",
+  "https://miniblox.online/textures/entity/skeleton/skeleton.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/skeleton.png",
+  "https://miniblox.online/textures/entity/slime/slime.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/slime.png",
+  "https://miniblox.online/textures/entity/spider/spider.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/spider.png",
+  "https://miniblox.online/textures/entity/zombie/zombie.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/zombie.png",
+  "https://miniblox.online/textures/entity/ghost/ghost.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/ghost.png",
+  "https://miniblox.online/textures/entity/zombie_cowman/zombie_cowman.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/zombie_cowman.png",
+  "https://miniblox.online/textures/entity/snowman/snowman.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/snowman.png",
+  "https://miniblox.online/textures/entity/arrow.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/arrow.png",
+  "https://miniblox.online/textures/entity/boat/acacia.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/acacia.png",
+  "https://miniblox.online/textures/entity/boat/birch.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/birch.png",
+  "https://miniblox.online/textures/entity/boat/dark_oak.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/dark_oak.png",
+  "https://miniblox.online/textures/entity/boat/jungle.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/jungle.png",
+  "https://miniblox.online/textures/entity/boat/oak.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/oak.png",
+  "https://miniblox.online/textures/entity/boat/spruce.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/spruce.png",
+  "https://miniblox.online/textures/models/armor/chainmail_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/chainmail_layer_1.png",
+  "https://miniblox.online/textures/models/armor/chainmail_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/chainmail_layer_2.png",
+  "https://miniblox.online/textures/models/armor/diamond_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/diamond_layer_1.png",
+  "https://miniblox.online/textures/models/armor/diamond_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/diamond_layer_2.png",
+  "https://miniblox.online/textures/models/armor/emerald_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/emerald_layer_1.png",
+  "https://miniblox.online/textures/models/armor/emerald_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/emerald_layer_2.png",
+  "https://miniblox.online/textures/models/armor/infernium_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/infernium_layer_1.png",
+  "https://miniblox.online/textures/models/armor/infernium_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/infernium_layer_2.png",
+  "https://miniblox.online/textures/models/armor/gold_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/gold_layer_1.png",
+  "https://miniblox.online/textures/models/armor/gold_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/gold_layer_2.png",
+  "https://miniblox.online/textures/models/armor/iron_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/iron_layer_1.png",
+  "https://miniblox.online/textures/models/armor/iron_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/iron_layer_2.png",
+  "https://miniblox.online/textures/models/armor/leather_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/leather_layer_1.png",
+  "https://miniblox.online/textures/models/armor/leather_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/leather_layer_2.png",
+  "https://miniblox.org/textures/spritesheet.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/spritesheet.png",
+  "https://miniblox.org/assets/Miniblox-Regular-Lz-C8nWS.otf": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/assets/Miniblox-Regular-Lz-C8nWS.otf",
+  "https://miniblox.org/textures/entity/chicken/chicken.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/chicken.png",
+  "https://miniblox.org/textures/entity/cow/cow.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/cow.png",
+  "https://miniblox.org/textures/entity/creeper/creeper.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/creeper.png",
+  "https://miniblox.org/textures/entity/pig/pig.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/pig.png", 
+  "https://miniblox.org/textures/entity/sheep/sheep_fur.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/sheep_fur.png",
+  "https://miniblox.org/textures/entity/skeleton/skeleton.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/skeleton.png",
+  "https://miniblox.org/textures/entity/slime/slime.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/slime.png",
+  "https://miniblox.org/textures/entity/spider/spider.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/spider.png",
+  "https://miniblox.org/textures/entity/zombie/zombie.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/zombie.png",
+  "https://miniblox.org/textures/entity/ghost/ghost.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/ghost.png",
+  "https://miniblox.org/textures/entity/zombie_cowman/zombie_cowman.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/zombie_cowman.png",
+  "https://miniblox.org/textures/entity/snowman/snowman.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/snowman.png",
+  "https://miniblox.org/textures/entity/arrow.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/arrow.png",
+  "https://miniblox.org/textures/entity/boat/acacia.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/acacia.png",
+  "https://miniblox.org/textures/entity/boat/birch.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/birch.png",
+  "https://miniblox.org/textures/entity/boat/dark_oak.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/dark_oak.png",
+  "https://miniblox.org/textures/entity/boat/jungle.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/jungle.png",
+  "https://miniblox.org/textures/entity/boat/oak.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/oak.png",
+  "https://miniblox.org/textures/entity/boat/spruce.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/entity/spruce.png",
+  "https://miniblox.org/textures/models/armor/chainmail_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/chainmail_layer_1.png",
+  "https://miniblox.org/textures/models/armor/chainmail_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/chainmail_layer_2.png",
+  "https://miniblox.org/textures/models/armor/diamond_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/diamond_layer_1.png",
+  "https://miniblox.org/textures/models/armor/diamond_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/diamond_layer_2.png",
+  "https://miniblox.org/textures/models/armor/emerald_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/emerald_layer_1.png",
+  "https://miniblox.org/textures/models/armor/emerald_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/emerald_layer_2.png",
+  "https://miniblox.org/textures/models/armor/infernium_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/infernium_layer_1.png",
+  "https://miniblox.org/textures/models/armor/infernium_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/infernium_layer_2.png",
+  "https://miniblox.org/textures/models/armor/gold_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/gold_layer_1.png",
+  "https://miniblox.org/textures/models/armor/gold_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/gold_layer_2.png",
+  "https://miniblox.org/textures/models/armor/iron_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/iron_layer_1.png",
+  "https://miniblox.org/textures/models/armor/iron_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/iron_layer_2.png",
+  "https://miniblox.org/textures/models/armor/leather_layer_1.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/leather_layer_1.png",
+  "https://miniblox.org/textures/models/armor/leather_layer_2.png": "https://raw.githubusercontent.com/heythereu/Miniblox-Textures/main/armor/leather_layer_2.png"
+};
+
+let rules = [];
+let idx = 1;
+
+for (const [src, dst] of Object.entries(URL_MAP)) {
+  rules.push({
+    "id": idx++,
+    "action": {
+      "type": "redirect",
+      "redirect": { "url": dst }
+    },
+    "condition": {
+      "urlFilter": src,
+      "resourceTypes": src.endsWith(".otf") ? ["font"] : ["image"]
+    }
+  });
+}
+
+chrome.declarativeNetRequest.updateDynamicRules(
+  {
+    addRules: rules,
+    removeRuleIds: rules.map(rule => rule.id)
+  },
+  () => {
+    if (chrome.runtime.lastError) {
+      console.error("Error updating:", chrome.runtime.lastError);
+    } else {
+      console.log("Rules updated");
+    }
+  }
+);
